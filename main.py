9b6282a24da1f472e51a040e7c14e30ea0d367dd
lucasrodlima <lucasrodlima27@gmail.com>
2025-06-19 03:01:36 -0300

    feat: main counts tokens and takes argument as prompt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -7,7 +8,13 @@ api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
-client_response = client.models.generate_content(model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+if len(sys.argv) == 2:
+    prompt = sys.argv[1]
+else:
+    sys.exit("Error: no prompt")
+
+client_response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
 
 print(client_response.text)
-
+print(f"Prompt tokens: {client_response.usage_metadata.prompt_token_count}")
+print(f"Response tokens: {client_response.usage_metadata.candidates_token_count}")
