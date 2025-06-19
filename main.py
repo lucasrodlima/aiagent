923d01c727013cc602bfed6a34bdf8b845153ace
lucasrodlima <lucasrodlima27@gmail.com>
2025-06-19 20:00:51 -0300

    feat: add client response to messages list

--- main.py
+++ main.py
@@ -2,18 +2,22 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
-if len(sys.argv) == 2:
+# if len(sys.argv) == 2:
+try:
     prompt = sys.argv[1]
-else:
+except:
     sys.exit("Error: no prompt")
 
-client_response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
+messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
+
+client_response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
 
 print(client_response.text)
 print(f"Prompt tokens: {client_response.usage_metadata.prompt_token_count}")
