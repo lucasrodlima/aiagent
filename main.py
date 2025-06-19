0de02efce41f61067d3fc83c6fca234a6d6def5b
lucasrodlima <lucasrodlima27@gmail.com>
2025-06-19 20:11:58 -0300

    feat: add verbose flag to main

--- main.py
+++ main.py
@@ -11,14 +11,17 @@ client = genai.Client(api_key=api_key)
 
 # if len(sys.argv) == 2:
 try:
-    prompt = sys.argv[1]
+    user_prompt = sys.argv[1]
 except:
     sys.exit("Error: no prompt")
 
-messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
+messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
 
 client_response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
 
 print(client_response.text)
-print(f"Prompt tokens: {client_response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {client_response.usage_metadata.candidates_token_count}")
+
+if  "--verbose" in sys.argv:
+    print(f"User prompt: {user_prompt}")
+    print(f"Prompt tokens: {client_response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {client_response.usage_metadata.candidates_token_count}")
